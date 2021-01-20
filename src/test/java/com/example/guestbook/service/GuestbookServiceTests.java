87e86ef2a976c4e76a537f3c5245e4b5bb12c02c
NHN <nhn@NHNui-MacBookPro.local>
2021-01-20 18:01:22 +0900

    part4.5

--- src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
@@ -1,5 +1,7 @@
 package com.example.guestbook.service;
 
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.dto.PageResultDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class GuestbookServiceTests {
     @Autowired
     private GuestbookService service;
 
+
     @Test
     public void testRegister(){
         GuestbookDTO guestbookDTO = GuestbookDTO.builder()
@@ -19,5 +22,31 @@ public class GuestbookServiceTests {
                 .writer("user0")
                 .build();
         System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList1(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+    }
+
+    @Test
+    public void testList2(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+        System.out.println("-------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList())
+            System.out.println(guestbookDTO);
+
+        System.out.println("===============================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 }
