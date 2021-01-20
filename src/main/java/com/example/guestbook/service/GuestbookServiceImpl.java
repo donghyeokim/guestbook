ba0b58e10aa116463f0bc8ab56895b8bcc841ce6
NHN <nhn@NHNui-MacBookPro.local>
2021-01-20 16:40:05 +0900

    part4.4

--- src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -1,16 +1,28 @@
 package com.example.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import com.example.guestbook.entity.Guestbook;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
-public class GuestbookServiceImpl {
+@RequiredArgsConstructor //의존성 자동 주
+public class GuestbookServiceImpl implements GuestbookService {
 
+    private final GuestbookRepository repository;
     @Override
     public Long register(GuestbookDTO dto){
-        return null;
+        log.info("DTO----------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
+        repository.save(entity);
+        return entity.getGno();
+
     }
 }
