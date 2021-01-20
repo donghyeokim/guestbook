7c51441e9bad62064f7c9c370d51e096420aa914
NHN <nhn@NHNui-MacBookPro.local>
2021-01-20 18:22:50 +0900

    part4.5

--- src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,17 +1,32 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.entity.Guestbook;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.service.GuestbookService;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor //자동 주입?을 위한
 public class GuestbookController {
-    @GetMapping({"/","/list"})
-    public String list(){
-        log.info("list...............");
-        return "/guestbook/list";
+
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list.............." + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
